f49d35bb152ae28f039592bced3605e3901e63c9
Coding-king <59760987+Katie27-maker@users.noreply.github.com>
2024-04-30 23:31:26 +0900

    Delete Bt64_JPA_kanban directory

--- Bt64_JPA_kanban/src/main/java/com/lec/spring/listener/Auditable.java
+++
@@ -1,8 +0,0 @@
-package com.lec.spring.listener;
-
-import java.time.LocalDateTime;
-
-public interface Auditable {
-    LocalDateTime getRegDate();
-    void setRegDate(LocalDateTime regDate);
-}
--- Bt64_JPA_kanban/src/main/resources/static/js/list.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("[name='pageRows']").change(function(){
-        let frm = $("[name='frmPageRows']");
-        frm.attr("method", "POST")
-        frm.attr("action", "pageRows")
-        frm.submit();
-    });
-});
