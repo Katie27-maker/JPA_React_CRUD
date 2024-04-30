46b603e6f360afaf01b151d8d2b961613b452c6e
Katie27-maker <wbdl2747@gmail.com>
2024-04-30 23:58:44 +0900

    kanban 추가

---
+++ bt70_JPA_KANBAN/src/main/java/com/lec/bt70_JPA_KANBAN/Bt70JpaKanbanApplication.java
@@ -0,0 +1,13 @@
+package com.lec.bt70_JPA_KANBAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt70JpaKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt70JpaKanbanApplication.class, args);
+	}
+
+}
---
+++ bt70_JPA_KANBAN/src/test/java/com/lec/bt70_JPA_KANBAN/Bt70JpaKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.bt70_JPA_KANBAN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt70JpaKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
